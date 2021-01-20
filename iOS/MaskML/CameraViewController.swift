14bcf540f361db242c3ace076549c3dc2555b2bd
Artemas J. Radik <ajradik@gmail.com>
2021-01-20 13:50:21 -0500

    back camera working with aspect fill

--- iOS/MaskML/CameraViewController.swift
+++ iOS/MaskML/CameraViewController.swift
@@ -5,7 +5,6 @@
 //  Created by Nicholas Bourdakos on 2/21/19.
 //  Copyright © 2019 Nicholas Bourdakos. All rights reserved.
 //
-
 import UIKit
 import CoreML
 import Vision
@@ -20,7 +19,7 @@ class CameraViewController: UIViewController {
     
     // MARK: - Variable Declarations
     
-    let ssdPredictor = try! SSDPredictor(Model(configuration: .init()).model)
+    let ssdPredictor = SSDPredictor(Model().model)
     var boundingBoxes: [UIBoundingBox] = []
     var screenHeight: CGFloat?
     var screenWidth: CGFloat?
@@ -29,19 +28,12 @@ class CameraViewController: UIViewController {
     
     let videoOutput = AVCaptureVideoDataOutput()
     lazy var captureSession: AVCaptureSession? = {
-        
-        guard let frontCamera = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera], mediaType: .video, position: AVCaptureDevice.Position.front).devices.first, let input = try? AVCaptureDeviceInput(device: frontCamera) else {
-            return nil
+        guard let backCamera = AVCaptureDevice.default(for: .video),
+            let input = try? AVCaptureDeviceInput(device: backCamera) else {
+                return nil
         }
         
-//        frontCamera.activeFormat.formatDescription.dimensions = CMVideoDimensions(width: screenWidth, height: screenHeight)
-        
-//        guard let backCamera = AVCaptureDevice.default(for: .video),
-//            let input = try? AVCaptureDeviceInput(device: backCamera) else {
-//                return nil
-//        }
-        
-        let dimensions = CMVideoFormatDescriptionGetDimensions(frontCamera.activeFormat.formatDescription)
+        let dimensions = CMVideoFormatDescriptionGetDimensions(backCamera.activeFormat.formatDescription)
         videoHeight = dimensions.height
         videoWidth = dimensions.width
         
@@ -51,22 +43,15 @@ class CameraViewController: UIViewController {
         
         if captureSession.canAddOutput(videoOutput) {
             let videoOutput = AVCaptureVideoDataOutput()
-
-            let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-            previewLayer.videoGravity = .resizeAspectFill
-            previewLayer.frame = CGRect(x: view.bounds.minX, y: view.bounds.minY, width: view.bounds.width, height: view.bounds.height)
-            // `.resize` allows the camera to fill the screen on the iPhone X.
-            previewLayer.connection?.videoOrientation = .portrait
-            cameraView.layer.addSublayer(previewLayer)
-            
-            if #available(iOS 13.0, *) {
-                videoOutput.automaticallyConfiguresOutputBufferDimensions = false
-                videoOutput.deliversPreviewSizedOutputBuffers = true
-            }
-            
             videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "MyQueue"))
             captureSession.addOutput(videoOutput)
-
+            
+            let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            previewLayer.frame = CGRect(x: view.bounds.minX, y: view.bounds.minY, width: view.bounds.width, height: view.bounds.height)
+            // `.resize` allows the camera to fill the screen on the iPhone X.
+            previewLayer.videoGravity = .resizeAspectFill
+            previewLayer.connection?.videoOrientation = .portrait
+            cameraView.layer.addSublayer(previewLayer)
             return captureSession
         }
         return nil
@@ -100,29 +85,21 @@ extension CameraViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
                 guard let screenWidth = self.screenWidth, let screenHeight = self.screenHeight, let videoWidth = self.videoWidth, let videoHeight =  self.videoHeight else {
                     return
                 }
-                
-//                let pixelBufferWidth = CVPixelBufferGetWidth(pixelBuffer)
-//                let pixelBufferHeight = CVPixelBufferGetHeight(pixelBuffer)
-                
                 let topKPredictions = predictions.prefix(self.boundingBoxes.count)
                 for (index, prediction) in topKPredictions.enumerated() {
                     guard let label = prediction.labels.first else {
                         return
                     }
                     
-                    let height = screenHeight
-                    let width = (CGFloat(videoHeight)/CGFloat(videoWidth)) * screenHeight //SCALING FIX! // Figure out why this works...
-                    // TODO: FIX
+                    let width = screenWidth
+                    let height = width * (CGFloat(videoWidth) / CGFloat(videoHeight))
                     let offsetY = (screenHeight - height) / 2
-                    let offsetX = (width - screenWidth) / 2 //SCALING FIX!
                     let scale = CGAffineTransform.identity.scaledBy(x: width, y: height)
-                    let transform = CGAffineTransform(scaleX: 1, y: -1).translatedBy(x: offsetX, y: -height - offsetY)
+                    let transform = CGAffineTransform(scaleX: 1, y: -1).translatedBy(x: 0, y: -height - offsetY)
                     let rect = prediction.boundingBox.applying(scale).applying(transform)
                     
-                    let modifiedRect = CGRect(x: width - rect.maxX, y: rect.minY, width: rect.width, height: rect.height) //For Front Camera
-                    
                     let color = UIColor(red: 36/255, green: 101/255, blue: 255/255, alpha: 1.0)
-                    self.boundingBoxes[index].show(frame: modifiedRect, label: label.identifier, color: color)
+                    self.boundingBoxes[index].show(frame: rect, label: label.identifier, color: color)
                 }
                 for index in topKPredictions.count ..< 20 {
                     self.boundingBoxes[index].hide()
@@ -131,4 +108,3 @@ extension CameraViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
         }
     }
 }
-
